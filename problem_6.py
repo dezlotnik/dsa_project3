2e13935a9d45721f059da33210379892c4088ebd
David Zlotnik <dzlotnik@9267-laptop.corp.zooxlabs.com>
2023-12-19 16:28:55 -0800

    add more test cases

--- problem_6.py
+++ problem_6.py
@@ -21,3 +21,9 @@ l = [i for i in range(0, 10)]  # a list containing 0 - 9
 random.shuffle(l)
 
 print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+
+l = [0,0]
+print ("Pass" if ((0, 0) == get_min_max(l)) else "Fail")
+
+l = [-2,-1]
+print ("Pass" if ((-2, -1) == get_min_max(l)) else "Fail")
